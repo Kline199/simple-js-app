8f9b90d1bee9eb07fd97484698cb23a951c3ae01
Kline199 <137530128+Kline199@users.noreply.github.com>
2023-09-29 12:02:33 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 alert('Hello world');
 
-// Create a variable named favoriteFood and assign it a value
-var favoriteFood = "Pizza";
+
+var favoriteFood = "Tacos";
 
 // Use document.write() to display the value of favoriteFood
 document.write("My favorite food is: " + favoriteFood);
