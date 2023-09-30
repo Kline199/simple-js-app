61d34ed1767015e1cfd824ab0ce7dac751376abd
Kline199 <137530128+Kline199@users.noreply.github.com>
2023-09-30 17:21:13 -0500

    Update scripts.js added three pokemon to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
+let pokemonList = [
+
+{name: "Bulbasaur",
+  height: 7,
+  types: ["grass", "poison"]};
+
+{name: "Charizard",
+  height: 17,
+  types: ["fire", "flying"]};
+
+{ name: "Pikachu",
+  height: 4,
+  types: ["electric"]};
+
+];
+
+
+
+
 
