cd7867beff1a9ab28c408570f419037b9570bb4d
Kline199 <137530128+Kline199@users.noreply.github.com>
2023-10-04 09:16:40 -0500

    Cleaned up formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
 let pokemonList = [
-{name: 'Bulbasaur',
-  height: 7,
-  types: ['grass', 'poison']},
-{name: 'Charizard',
-  height: 17,
-  types: ['fire', 'flying']},
-{name: 'Beedrill',
-  height: 4,
-  types: ['bug', 'poison']},
+  {name: "Bulbasaur", height: 7, types: ["grass", "poison"]},
+  {name: "Charizard", height: 17, types: ["fire", "flying"]},
+  {name: "Beedrill", height: 4, types: ["bug", "poison"]},
 ];
-
-
 
 
 
