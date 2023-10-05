d02097462fd34af166b9d0a8085cacc37250fd46
Kline199 <137530128+Kline199@users.noreply.github.com>
2023-10-05 10:37:42 -0500

    Created loop for pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 let pokemonList = [
-  {name: "Bulbasaur", height: 7, types: ["grass", "poison"]},
-  {name: "Charizard", height: 17, types: ["fire", "flying"]},
-  {name: "Beedrill", height: 4, types: ["bug", "poison"]},
+  {name: "Bulbasaur", height: 0.7, types: ["grass", "poison"]},
+  {name: "Charizard", height: 1.7, types: ["fire", "flying"]},
+  {name: "Beedrill", height: 0.4, types: ["bug", "poison"]},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.0) {
+    document.write(
+      pokemonList[i].name +
+        " (height: " +
+        pokemonList[i].height +
+        " ) - Wow, that's big!<br>",
+    );
+  } else {
+    document.write(
+      pokemonList[i].name + " (height: " + pokemonList[i].height + " )<br>",
+    );
+  }
+}
+
 
 
 
