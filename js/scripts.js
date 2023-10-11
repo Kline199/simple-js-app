c890a3067c12a534dee343a99ce512f8e2d19daf
Kline199 <seankline1@gmail.com>
2023-10-10 20:43:00 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 let pokemonList = [
-  {name: "Bulbasaur", height: 0.7, types: ["grass", "poison"]},
-  {name: "Charizard", height: 1.7, types: ["fire", "flying"]},
-  {name: "Beedrill", height: 0.4, types: ["bug", "poison"]},
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
+  { name: "Beedrill", height: 0.4, types: ["bug", "poison"] },
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.0)
-   {document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "- Wow. That\'s big!"+ "<br>"); 
-} else {
-document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
+  if (pokemonList[i].height > 1.0) {
+    document.write(
+      pokemonList[i].name +
+        "  " +
+        "(height:" +
+        "  " +
+        pokemonList[i].height +
+        " ) " +
+        "- Wow. That's big!" +
+        "<br>",
+    );
+  } else {
+    document.write(
+      pokemonList[i].name +
+        "  " +
+        "(height:" +
+        "  " +
+        pokemonList[i].height +
+        " ) " +
+        "<br>",
+    );
+  }
 }
-
-
-
-
