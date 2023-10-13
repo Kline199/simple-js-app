bd9759f6dc0befeb3f6c5f4a9a35586f3d17138f
Kline199 <137530128+Kline199@users.noreply.github.com>
2023-10-13 11:49:45 -0500

    task 1.5
    - replace for with forEach
    - wrapped array in IIFE
    - new pokemon repository
    - return objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,43 @@
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
-  { name: "Beedrill", height: 0.4, types: ["bug", "poison"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
+    { name: "Beedrill", height: 0.4, types: ["bug", "poison"] },
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.0) {
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      pokemonList.push(pokemon);
+    } 
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+pokemonRepository.add({ name: "Pikachu", height: 0.4, types: ["electric"] });
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  if (pokemon.height > 1.0) {
     document.write(
-      pokemonList[i].name +
-        "  " +
-        "(height:" +
-        "  " +
-        pokemonList[i].height +
-        " ) " +
-        "- Wow. That's big!" +
-        "<br>",
+      pokemon.name +
+        " (height: " +
+        pokemon.height +
+        ") - Wow. That's big!<br>"
     );
   } else {
     document.write(
-      pokemonList[i].name +
-        "  " +
-        "(height:" +
-        "  " +
-        pokemonList[i].height +
-        " ) " +
-        "<br>",
+      pokemon.name +
+        " (height: " +
+        pokemon.height +
+        ")<br>"
     );
   }
-}
+});
+
