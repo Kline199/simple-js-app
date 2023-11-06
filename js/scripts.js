e1d92fcffb08df1f3c78677d44193a1fd3a31db8
Kline199 <137530128+Kline199@users.noreply.github.com>
2023-11-06 14:57:33 -0600

    list items added per task

--- js/scripts.js
+++ js/scripts.js
@@ -12,32 +12,43 @@ let pokemonRepository = (function () {
   function add(pokemon) {
     if (typeof pokemon === "object") {
       pokemonList.push(pokemon);
-    } 
+    }
+  }
+
+  function addListItem(pokemon) {
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('custom-button');
+    listItem.appendChild(button);
+    ul.appendChild(listItem);
+
+    // Task 2: Add an event listener to the button
+    button.addEventListener('click', function () {
+      // Task 3: Call the showDetails function, passing the pokemon object as a parameter
+      showDetails(pokemon);
+    });
+  }
+
+  // Task 1: Create the showDetails function
+  function showDetails(pokemon) {
+    console.log(pokemon); 
   }
 
   return {
     getAll: getAll,
     add: add,
+    addListItem: addListItem,
   };
 })();
+
+
+const ul = document.querySelector('.pokemon-list');
+
+ul.innerHTML = '';
 
 pokemonRepository.add({ name: "Pikachu", height: 0.4, types: ["electric"] });
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  if (pokemon.height > 1.0) {
-    document.write(
-      pokemon.name +
-        " (height: " +
-        pokemon.height +
-        ") - Wow. That's big!<br>"
-    );
-  } else {
-    document.write(
-      pokemon.name +
-        " (height: " +
-        pokemon.height +
-        ")<br>"
-    );
-  }
+  pokemonRepository.addListItem(pokemon);
 });
-
